ec2b4280f77d48655680a69675b64438ec5410bb
tingqianlin <diyidea921@gmail.com>
2018-04-13 10:52:03 +0800

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-# jsfp
-JavaScript Function Programming
+# JavaScript Function Programming
+
+Following the course [_Functional Programming for Beginners with JavaScript_](https://www.udemy.com/functional-programming-for-beginners-with-javascript) which created by [_James Moore_](https://knowthen.com)
+
+Practice Function Programming with JavaScript, [Ramda](https://ramdajs.com), [HyperScript](https://github.com/hyperhype/hyperscript), [HyperScript Helpers](https://github.com/ohanhi/hyperscript-helpers).
---
+++ counter/src/index.js
@@ -0,0 +1,48 @@
+import h from 'hyperscript'
+import hh from 'hyperscript-helpers'
+
+const { div, button } = hh(h);
+
+const initModel = 0;
+
+function view(dispatch, model) {
+    return div([
+        div({ className: 'mv2' }, `Count: ${model}`),
+        button({ className: 'pv1 ph2 mr2',
+            onclick: () => dispatch(MSGS.ADD) }, '+'),
+        button({ className: 'pv1 ph2',
+            onclick: () => dispatch(MSGS.SUB) }, '-'),
+    ]);
+}
+
+const MSGS = {
+    ADD: '+',
+    SUB: '-',
+};
+
+function update(msg, model) {
+    switch (msg) {
+        case MSGS.ADD:
+            return model + 1;
+        case MSGS.SUB:
+            return model - 1;
+        default:
+            return model;
+    }
+}
+
+function app(initModel, update, view, node) {
+    let model = initModel;
+    let currentView = view(dispatch, model);
+    node.appendChild(currentView);
+    function dispatch(msg) {
+        model = update(msg, model);
+        const updatedView = view(dispatch, model);
+        node.replaceChild(updatedView, currentView);
+        currentView = updatedView;
+    }
+}
+
+const rootNode = document.getElementById('app');
+
+app(initModel, update, view, rootNode);
