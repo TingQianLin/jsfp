5d52b6a96617896732099f97f697d62a0047b6b1
tingqianlin <diyidea921@gmail.com>
2018-04-13 11:47:40 +0800

    Fix appendChild missing bug

--- counter/src/index.js
+++ counter/src/index.js
@@ -36,6 +36,7 @@ function app(initModel, update, view, node) {
     let model = initModel;
     let currentView = view(dispatch, model);
     let rootNode = createElement(currentView);
+    node.appendChild(rootNode);
     function dispatch(msg) {
         model = update(msg, model);
         const updatedView = view(dispatch, model);
