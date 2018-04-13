a8db0cb77b4f69c2997b9ff6aa4c01de9515370d
tingqianlin <diyidea921@gmail.com>
2018-04-13 16:45:52 +0800

    Create caloriecounter app

---
+++ caloriecounter/README.md
@@ -0,0 +1,7 @@
+# Calorie Counter
+
+A Calorie Counter.
+
+## Initial Steps
+
+1. Run `npm install` to install the required modules.
---
+++ caloriecounter/src/Model.js
@@ -0,0 +1,10 @@
+const initModel = {
+    description: '',
+    calories: 0,
+    showFrom: false,
+    nextId: 0,
+    editId: null,
+    meals: [],
+};
+
+export default initModel;
